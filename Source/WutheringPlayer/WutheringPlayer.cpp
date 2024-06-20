8f83ba61275fd32dea8238aa30f4bd5606801907
g0r0kke <jiniandjuni@gmail.com>
2024-06-20 19:47:30 +0900

    240620 플레이어 이동

---
+++ Source/WutheringPlayer/WutheringPlayer.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WutheringPlayer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WutheringPlayer, "WutheringPlayer" );
