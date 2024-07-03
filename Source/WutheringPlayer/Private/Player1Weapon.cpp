22546ce4a3e81c4fb5eb232c8b802d25331c0709
g0r0kke <jiniandjuni@gmail.com>
2024-07-03 15:23:39 +0900

    칼 장착

--- Source/WutheringPlayer/Private/Player1Weapon.cpp
+++ Source/WutheringPlayer/Private/Player1Weapon.cpp
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Player1Weapon.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/SceneComponent.h"
+#include "UObject/ConstructorHelpers.h"
 
 // Sets default values
 APlayer1Weapon::APlayer1Weapon()
@@ -9,10 +11,14 @@ APlayer1Weapon::APlayer1Weapon()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// 기본 씬 컴포넌트 등록
+	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+	RootComponent = SceneComponent;
+
 	// 무기 스태틱메시 컴포넌트 등록
 	weaponMeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("weaponMeshComp"));
 	// 부모 컴포넌트를 Mesh 컴포넌트로 설정
-	RootComponent = weaponMeshComp;
+	weaponMeshComp->SetupAttachment(SceneComponent);
 
 	// 스태틱메시 데이터 로드
 	ConstructorHelpers::FObjectFinder<USkeletalMesh> TempWeaponMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Asset/InfinityBladeWeapons/Weapons/Dual_Blade/Dual_Blade_WrappedDeath/SK_Dual_Blade_WrappedDeath.SK_Dual_Blade_WrappedDeath'"));
